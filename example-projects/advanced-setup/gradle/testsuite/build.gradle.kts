07767151da774173c7c4fec3f5ab946eb5f160b4
mlsandnerCI <maren.sandner@code-intelligence.com>
2024-04-10 15:34:06 +0200

    feat: update gradle plugin version in example-projects

--- example-projects/advanced-setup/gradle/testsuite/build.gradle.kts
+++ example-projects/advanced-setup/gradle/testsuite/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     id("java-library")
-    id("com.code-intelligence.cifuzz") version "1.16.0"
+    id("com.code-intelligence.cifuzz") version "1.17.0"
 }
 
 repositories {
