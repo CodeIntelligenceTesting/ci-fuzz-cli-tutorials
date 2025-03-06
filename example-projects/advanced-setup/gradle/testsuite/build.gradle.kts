1684cad1e9a1c234312b9567713d9c6784390e79
Jochen <jochen.hilgers@gmail.com>
2025-03-06 10:36:09 +0100

    build(gradle): use public plugin version instead of the one from the private repository

--- example-projects/advanced-setup/gradle/testsuite/build.gradle.kts
+++ example-projects/advanced-setup/gradle/testsuite/build.gradle.kts
@@ -1,21 +1,10 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     id("java-library")
-    id("com.code-intelligence.cifuzz") version "1.17.0"
+    id("com.code-intelligence.cifuzz") version "1.23.2"
 }
 
 repositories {
-	maven {
-		name = "CodeIntelligenceRepository"
-		url = uri("https://gitlab.code-intelligence.com/api/v4/projects/89/packages/maven")
-		credentials {
-			username = extra["CodeIntelligenceRepositoryUsername"].toString()
-			password = extra["CodeIntelligenceRepositoryPassword"].toString()
-		}
-		content {
-			includeGroupByRegex("com\\.code-intelligence.*")
-		}
-	}
 	mavenCentral()
 }
 
--- example-projects/simple-setup/gradle-kotlin/build.gradle.kts
+++ example-projects/simple-setup/gradle-kotlin/build.gradle.kts
@@ -1,22 +1,10 @@
 plugins {
 	application
 	id("org.jetbrains.kotlin.jvm") version "1.7.20"
-	id("com.code-intelligence.cifuzz") version "1.17.0"
+	id("com.code-intelligence.cifuzz") version "1.23.2"
 }
 
 repositories {
-	// Configure access to CI repository
-	maven {
-		name = "CodeIntelligenceRepository"
-		url = uri("https://gitlab.code-intelligence.com/api/v4/projects/89/packages/maven")
-		credentials {
-			username = extra["CodeIntelligenceRepositoryUsername"].toString()
-			password = extra["CodeIntelligenceRepositoryPassword"].toString()
-		}
-		content {
-			includeGroupByRegex("com\\.code-intelligence.*")
-		}
-	}
 	mavenCentral()
 }
 
