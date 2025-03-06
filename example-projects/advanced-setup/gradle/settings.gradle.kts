cb0a8e4693c4a767d7e6d4a3e1fa6dd1478f7ecf
Jochen <jochen.hilgers@gmail.com>
2025-03-06 10:36:09 +0100

    build(gradle): use public plugin version instead of the one from the private repository

--- example-projects/advanced-setup/gradle/settings.gradle.kts
+++ example-projects/advanced-setup/gradle/settings.gradle.kts
@@ -1,19 +1,2 @@
-pluginManagement {
-	repositories {
-		maven {
-			name = "CodeIntelligenceRepository"
-			url = uri("https://gitlab.code-intelligence.com/api/v4/projects/89/packages/maven")
-			credentials {
-				username = extra["CodeIntelligenceRepositoryUsername"].toString()
-				password = extra["CodeIntelligenceRepositoryPassword"].toString()
-			}
-			content {
-				includeGroupByRegex("com\\.code-intelligence.*")
-			}
-		}
-		gradlePluginPortal()
-	}
-}
-
 include("app")
 include("testsuite")
